4eba744a4c879f73d3c8bd605b5cc1d4f7006721
user <user@user-PC>
2017-12-12 12:47:33 +0900

    aaaa

--- test_git/src/kr/co/sist/test/TestGit.java
+++ test_git/src/kr/co/sist/test/TestGit.java
@@ -5,6 +5,9 @@ public class TestGit {
 	public static void main(String[] args) {
 		int i=10;
 		System.out.println("Git Hub 연습"+i);
+		System.out.println("Git Hub 연습"+i);
+		System.out.println("Git Hub 연습"+i);
+		System.out.println("Git Hub 연습"+i);
 
 	}
 
